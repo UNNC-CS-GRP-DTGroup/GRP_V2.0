3bfe45f0916ecc4cb0053a55949a45911e33b318
PAN <zy14897@nottingham.edu.cn>
2015-12-05 22:09:02 +0800

    Create MyJTextField class and replace all JTextField with this new class

---
+++ GRP_V2.0/src/com/edu/nottingham/notetaking/MyJTextField.java
@@ -0,0 +1,51 @@
+package com.edu.nottingham.notetaking;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.Window;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JTextField;
+
+
+/**
+ * A modified JTextField which overrides paintComponent to fix a bug
+ * 
+ * @author Yichen PAN
+ * 
+ */
+
+public class MyJTextField extends JTextField {
+    private static final long serialVersionUID = 1L;
+    private BufferedImage buffer = null;
+
+    @Override public void paintComponent(Graphics g) {
+        Component window = this.getTopLevelAncestor();
+        if (window instanceof Window && !((Window)window).isOpaque()) {
+            // This is a translucent window, so we need to draw to a buffer
+            // first to work around a bug in the DirectDraw rendering in Swing.
+            int w = this.getWidth();
+            int h = this.getHeight();
+            if (buffer == null || buffer.getWidth() != w || buffer.getHeight() != h) {
+                // Create a new buffer based on the current size.
+                GraphicsConfiguration gc = this.getGraphicsConfiguration();
+                buffer = gc.createCompatibleImage(w, h, BufferedImage.TRANSLUCENT);
+            }
+
+            // Use the super class's paintComponent implementation to draw to
+            // the buffer, then write that buffer to the original Graphics object.
+            Graphics bufferGraphics = buffer.createGraphics();
+            try {
+                super.paintComponent(bufferGraphics);
+            } finally {
+                bufferGraphics.dispose();
+            }
+            g.drawImage(buffer, 0, 0, w, h, 0, 0, w, h, null);
+        } else {
+            // This is not a translucent window, so we can call the super class
+            // implementation directly.
+            super.paintComponent(g);
+        }        
+    }
+}
