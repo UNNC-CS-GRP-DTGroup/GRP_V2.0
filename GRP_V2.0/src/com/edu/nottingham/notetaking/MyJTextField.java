9e05195039973024b40b790596fcd0184c069125
Eason <PAN.Atopos@outlook.com>
2015-12-07 21:09:34 +0800

    Add a new constructor to MyJTextField class

--- GRP_V2.0/src/com/edu/nottingham/notetaking/MyJTextField.java
+++ GRP_V2.0/src/com/edu/nottingham/notetaking/MyJTextField.java
@@ -20,7 +20,12 @@ public class MyJTextField extends JTextField {
     private static final long serialVersionUID = 1L;
     private BufferedImage buffer = null;
 
-    @Override public void paintComponent(Graphics g) {
+    public MyJTextField(int columns) {
+    	super(null, null, columns);
+	}
+    
+    @Override 
+    public void paintComponent(Graphics g) {
         Component window = this.getTopLevelAncestor();
         if (window instanceof Window && !((Window)window).isOpaque()) {
             // This is a translucent window, so we need to draw to a buffer
